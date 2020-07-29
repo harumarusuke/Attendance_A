4fef5e063231332135f2ef2f55f20344c4635f4e
harumarusuke <harumarusuke@gmail.com>
2020-07-29 10:32:43 +0000

    top_page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  
+  def full_title(page_name = "") # メソッドと引数の定義
+    base_title = "AttendanceAApp"
+    if page_name.empty? # 引数を受け取っているか判定
+      base_title
+    else 
+      page_name + " | " + base_title
+    end
+  end
 end
