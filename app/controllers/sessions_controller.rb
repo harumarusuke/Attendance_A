1741f953440ba6ab51a24ff2ef347e48dca6af93
harumarusuke <harumarusuke@gmail.com>
2020-07-30 11:33:23 +0000

    full-login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
+      params[:sessions][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user
     else
       flash.now[:danger] = "ログイン出来ませんでした。"
@@ -15,9 +16,9 @@ class SessionsController < ApplicationController
   end
   
   def destroy
-    log_out
+    log_out if logged_in?
     flash[:success] = 'ログアウトしました。'
-    redirect_to root_path
+    redirect_to root_url
   end
   
 end
